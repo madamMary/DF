4287277652543ed2eafd78d4d7ea9151cf42af9f
madamMary <120038116+madamMary@users.noreply.github.com>
2023-07-02 19:43:41 +0400

    Delete Desktop/gogfood2 directory

--- Desktop/gogfood2/src/components/General/index.jsx
+++
@@ -1,7 +0,0 @@
-import Logo from "./Logo";
-import Header from "./Header";
-import Footer from "./Footer";
-
-import "./style.css";
-
-export {Logo, Header, Footer};
--- Desktop/gogfood2/src/pages/Favorites.jsx
+++
@@ -1,9 +0,0 @@
-import Card from "../components/Card";
-
-const Favorites = ({goods, userId, setServerGoods}) => {
-return <div className="container">
-    {goods.filter(el => el.likes.includes(userId)).map (g => <Card {...g} key={g._id} img={g.pictures} setServerGoods={setServerGoods}/>)}
-</div>
-}
-
-export default Favorites;
